56efdc95d5d4601d63d14c45bc5ca45266da2443
Jess <jessbodie@gmail.com>
2018-04-12 18:49:53 -0400

    Add POST  /todos route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,23 @@ var {Todo} = require('./models/todo');
 
 var app = express();
 
+app.use(bodyParser.json());
+
+// Resource creation
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
