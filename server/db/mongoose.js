effab6f4844d31e60299b78cf84c23e2b7d8cd9f
Jess <jessbodie@gmail.com>
2018-04-13 16:27:57 -0400

    Set up app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 // Because Promises not originally part of  JS, need to specify
 mongoose.Promise = global.Promise;
 // Similar to MongoClient.connect
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose: mongoose
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const {User} = require('./models/user');
 const {Todo} = require('./models/todo');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -61,8 +62,8 @@ app.get('/todos/:id', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 
